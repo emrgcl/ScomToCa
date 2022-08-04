d999c3fb874e4625a71d489e3d867cc5589f314e
Emre <emreguclu@hotmail.com>
2022-08-04 16:35:45 +0300

    Updated

--- FilterTest.ps1
+++ FilterTest.ps1
@@ -12,6 +12,7 @@ Function Test-ClassName{
 $Config = Import-PowerShellDataFile -Path .\Config.psd1
 $AlertObjects = Import-Clixml -Path C:\temp\AlertObjects_9f78ae5d-e955-42b6-878b-ce95877178ac.xml
 $Result = $AlertObjects | Where-Object { (Test-ClassName -ClassNames $_.ClassNames -Config $Config) -or ($_.WorkflowName -in $Config.WorkflowNames) }
+,
 [PsCustomObject]@{
     TotalCount = $AlertObjects.Count
     FilteredCount = $Result.Count
